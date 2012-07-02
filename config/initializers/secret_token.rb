d7cdd3e4ad6fad7abf77d605aeb2efe899be0183
Oscar Rendon <oarendon@gmail.com>
2012-07-02 17:09:53 -0500

    initial commit with --skip-test-unit flag

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyShop::Application.config.secret_token = '675d64861e97284c3b3cb1380290d0ffa5f584c552529df702dfd14b3adf74a4595ae0082dcfdd5535eeb4d65052ce624cf761a98f458592a4dc06b96bb9d5b1'
