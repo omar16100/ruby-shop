2b7776a465bbe8c999b2bbb233d51642dc9710d8
Oscar Rendon <oarendon@gmail.com>
2012-07-03 10:44:39 -0500

    added default users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create!(email: "user@rubyshop.co", password: "123456", admin: false)
+User.create!(email: "admin@rubyshop.co", password: "123456", admin: true)
