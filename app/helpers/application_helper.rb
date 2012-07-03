eb12d112e405ca427a8b3ae88b374e3c7daf8c05
Oscar Rendon <oarendon@gmail.com>
2012-07-03 17:15:36 -0500

    fix menu options according to user role

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+	
+	def get_menu_options
+		menu_options = ['Home', 'Catalog']
+
+		if user_signed_in?
+			menu_options << 'Products'
+			menu_options << 'Admin' if current_user.admin?
+		end
+
+		menu_options << 'Profile'
+		menu_options << 'About'
+	end
 end
