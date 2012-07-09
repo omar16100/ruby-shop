524f763c5f8af9d94dd93d873aefa59177f41d9b
Oscar Rendon <oarendon@gmail.com>
2012-07-08 21:05:03 -0500

    updated for left sidebar across all pages

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,2 +1,11 @@
 module CategoriesHelper
+  def category_link(cat)
+    content_tag_for :li, cat, :style => "background-color: #{bg_color(cat)}" do
+      link_to cat.name, cat
+    end
+  end
+
+  def bg_color(cat)
+    cat.color
+  end
 end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,6 +2,14 @@ class Cart < ActiveRecord::Base
   
   attr_accessible :user_id
 
+  def total_price
+    sum = 0
+    products.each do |product|
+      sum =  sum + product.price
+    end
+    return sum
+  end
+
   belongs_to :user
   has_many :cart_items
   has_many :products, through: :cart_items
