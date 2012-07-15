e9f9254650c9fed5b25249616e2b03e258a88a51
Oscar Rendon <oarendon@gmail.com>
2012-07-15 15:23:17 -0500

    added CRUD for users in admin section

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,80 @@
+class Admin::UsersController < ApplicationController
+  
+  layout "backend"
+
+  # GET /admin/users
+  def index
+    @admin_users = User.all
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @admin_users }
+    end
+  end
+
+  # GET /admin/users/1
+  def show
+    @admin_user = User.find(params[:id])
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @admin_user }
+    end
+  end
+
+  # GET /admin/users/new
+  def new
+    @admin_user = User.new
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @admin_user }
+    end
+  end
+
+  # GET /admin/users/1/edit
+  def edit
+    @admin_user = User.find(params[:id])
+  end
+
+  # POST /admin/users
+  def create
+    @admin_user = User.new(params[:admin_user])
+
+    respond_to do |format|
+      if @admin_user.save
+        format.html { redirect_to @admin_user, notice: 'User was successfully created.' }
+        format.json { render json: @admin_user, status: :created, location: @admin_user }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @admin_user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /admin/users/1
+  def update
+    @admin_user = User.find(params[:id])
+
+    respond_to do |format|
+      if @admin_user.update_attributes(params[:admin_user])
+        format.html { redirect_to @admin_user, notice: 'User was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @admin_user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /admin/users/1
+  def destroy
+    @admin_user = User.find(params[:id])
+    @admin_user.destroy
+
+    respond_to do |format|
+      format.html { redirect_to admin_users_url }
+      format.json { head :no_content }
+    end
+  end
+end
