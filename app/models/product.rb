d0e767f86d22118600c8a1189a312ec1a4cffa25
Oscar Rendon <oarendon@gmail.com>
2012-07-30 14:19:45 -0500

    updated to include product image

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,10 +3,11 @@ class Product < ActiveRecord::Base
 
   friendly_id :name, use: :slugged
 
-  attr_accessible :description, :name, :on_sale, :price, :qty, :sale_price, :category, :category_id
+  attr_accessible :description, :name, :on_sale, :price, :qty, :sale_price,
+    :category, :category_id, :photo
   belongs_to :category
 
-  has_attached_file :photo, styles: { thumb: "150x150>", normal: "400x400>" }
+  has_attached_file :photo, styles: { mini: "80x80>", thumb: "175x175>", normal: "400x400>" }
 
   scope :in_stock, where("qty > 0")
   scope :by_category, lambda { |id| where(category_id: id) }
@@ -14,7 +15,7 @@ class Product < ActiveRecord::Base
   validates :name, :price, :qty, :presence => true
   validates :price, :sale_price, :qty, :numericality => true
   validates :category, :presence => true
-  
+
   validates_attachment :photo,
     content_type: { content_type: /^image\/.?(gif|png|jpg|jpeg)$/i },
     size: { in: 0..2000.kilobytes }
